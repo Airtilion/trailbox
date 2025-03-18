72370184f017b8c48f895f38c3bc5c53eef6410a
JcobV01 <jakubwadycki@gmail.com>
2025-03-18 22:22:52 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import videoBg from '../assets/video/test.mp4'
+import Navbar from './Navbar'
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full h-dvh relative'>
+      <Navbar />
+
+      <section className='absolute z-0 w-[calc(100%-100px)] h-[calc(100%-100px)] top-[32px] left-[50%] translate-x-[-50%] max-xl:w-[calc(100%-40px)] max-sm:w-[calc(100%-20px)]'>
+
+        <div className='h-full'>
+          <div className='w-[80%] h-[128px] absolute z-10 bg-white top-0 left-0 rounded-br-[40px] r-corners max-2xl:h-[112px] max-xl:h-[96px] max-xl:w-[83%] max-xl:rounded-br-[30px] max-lg:w-[30%] max-md:w-[50%] max-sm:w-[60%]' />
+          <div className='w-[270px] h-[75px] absolute z-20 bg-white bottom-0 right-0 rounded-tl-[40px] r-corners-b max-xl:rounded-tl-[30px] max-xl:w-[240px] max-md:hidden' />
+          <video className='cp object-cover brightness-30 rounded-[40px] h-full w-full z-0 max-xl:rounded-[30px]' autoPlay muted loop>
+            <source src={videoBg} type="video/mp4" />
+          </video>
+        </div>
+
+        <div className='absolute top-[48px] flex gap-[16px] z-20 right-0 w-[calc(100%-80%)] justify-center max-2xl:top-[36px] max-xl:gap-[8px] max-xl:top-[32px] max-xl:w-[calc(100%-83%)] max-lg:hidden'>
+          <div className='absolute w-[32px] h-[1px] bg-white top-[50%] translate-x-[-50%] left-[52px] max-2xl:left-[26px] max-2xl:w-[16px] max-xl:hidden'></div>
+          <Link to="#">
+            <Icon icon="twemoji:flag-poland" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+          </Link>
+          <Link to="#">
+            <Icon icon="twemoji:flag-united-kingdom" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+          </Link>
+          <Link to="#">
+            <Icon icon="twemoji:flag-germany" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+          </Link>
+          <div className='absolute w-[32px] h-[1px] bg-white top-[50%] translate-x-[-50%] right-[20px] max-2xl:w-[16px] max-2xl:right-[16px] max-xl:hidden'></div>
+        </div>
+
+        <div className='absolute top-[50%] left-[128px] max-xl:left-[64px] max-sm:left-[50%] max-sm:translate-x-[-50%] max-sm:w-[90%]'>
+          <h1 className='text-[70px] text-white font-extrabold z-20 max-xl:text-[55px] max-lg:text-[40px] max-sm:text-center max-sm:text-[30px]'>PŁYTY WARSTWOWE</h1>
+          <p className='text-[30px] text-white max-xl:text-[25px] max-lg:text-[20px] max-sm:text-center max-sm:text-[15px]'>dla wielu zastosowań</p>
+        </div>
+
+        <button className='w-[250px] h-[55px] rounded-full z-20 bg-[#898989] hover:bg-[#676767] duration-300 cursor-pointer absolute bottom-0 right-0 flex justify-center items-center gap-[8px] shadow-[0px_4px_20px_#00000040] max-xl:w-[220px] max-md:hidden'>
+          <Icon icon="solar:letter-bold" width="26" height="26" className="text-white max-xl:w-[24px]" />
+          <p className='text-white text-[20px] max-xl:text-[16px]'>Napisz do nas</p>
+        </button>
+
+        <div className='absolute bottom-[16px] left-[50%] translate-x-[-50%] flex flex-col items-center gap-[8px]'>
+          <div className='w-[25px] h-[50px] border-2 border-white rounded-full flex justify-center items-center relative bounce-lines'>
+            <div className='w-[15px] h-[15px] rounded-full bg-white animate-bounce'></div>
+          </div>
+          <p className='text-[15px] font-light text-white'>Dowiedz się więcej</p>
+        </div>
+      </section>
+
+    </header>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,116 @@
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/images/logo.svg'
+import { Icon } from '@iconify/react/dist/iconify.js'
+
+const elements = [
+    {
+        name: 'O nas',
+        link: '#onas',
+    },
+    {
+        name: 'Płyty sandwich',
+        link: '/plytysandwich',
+    },
+    {
+        name: 'Zestawy kontenerowe',
+        link: '/zestawy',
+    },
+    {
+        name: 'Usługa CNC',
+        link: '/cnc',
+    },
+    {
+        name: 'Realizacje',
+        link: '/realizacje',
+    },
+    {
+        name: 'Kontakt',
+        link: '#kontakt',
+    }
+]
+
+const Navbar = () => {
+    const menuRef = useRef(null);
+    const mobileMenuRef = useRef(null);
+
+    const menuVisibilityHandle = () => {
+        const mButton = menuRef.current;
+        const mMenu = mobileMenuRef.current;
+        mButton.classList.contains("active") ? mButton.classList.remove("active") : mButton.classList.add("active");
+        if (mMenu.classList.contains("flex")) {
+            mMenu.classList.remove("flex")
+            mMenu.classList.add("hidden")
+        }
+        else {
+            mMenu.classList.add("flex");
+            mMenu.classList.remove("hidden")
+        }
+    }
+
+    const handleResize = () => {
+        if (window.innerWidth > 1023) {
+            mobileMenuRef.current.classList.add("hidden");
+            mobileMenuRef.current.classList.remove("flex");
+            menuRef.current.classList.remove("active");
+        }
+    };
+
+    return (
+        <>
+            <div ref={menuRef} onClick={menuVisibilityHandle} className='hidden max-lg:flex hamburger-menu w-14 h-14 bg-white absolute flex-col justify-between items-center py-4 px-3 right-[48px] top-[64px] rounded-[10px] z-40 max-sm:right-[40px]'>
+                <span className='w-8 h-1 bg-[#898989] duration-500 rounded-full'></span>
+                <span className='w-8 h-1 bg-[#898989] duration-500 rounded-full'></span>
+                <span className='w-8 h-1 bg-[#898989] duration-500 rounded-full'></span>
+            </div>
+
+            <nav className='relative z-20 flex justify-between items-center w-[calc(80%-100px)] h-[128px] mx-[50px] pr-[32px] top-[32px] max-2xl:pr-[24px] max-2xl:h-[112px] max-xl:mx-[20px] max-xl:w-[calc(83%-40px)] max-xl:h-[96px] max-lg:w-[calc(30%-40px)] max-lg:justify-center max-lg:pr-0 max-md:w-[calc(50%-40px)] max-sm:w-[calc(60%-20px)] max-sm:mx-[10px]'>
+                <Link to="/">
+                    <img src={logo} alt="Logo trailbox" width={200} className='mt-[-30px] max-2xl:w-[150px] max-2xl:mt-[-20px] max-sm:w-[140px]' />
+                </Link>
+
+                <ul className='flex gap-[48px] max-2xl:gap-[28px] max-xl:gap-[24px] max-lg:hidden'>
+                    {elements.map((element, index) => (
+                        <li key={index} className='text-[16px] max-2xl:text-[15px] max-xl:text-[14px]'>
+                            <Link to={element.link}>{element.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* mobilki */}
+
+                <div ref={mobileMenuRef} className='menu-mobile fixed bottom-0 left-0 w-full h-dvh bg-[#000000c1] hidden backdrop-blur-[5px] flex-col gap-[32px] justify-center items-center z-20'>
+                    <ul className='flex flex-col gap-8'>
+                        {
+                            elements.map((item, index) => (
+                                <li key={index} style={{ animationDelay: `${index * 100}ms` }} onClick={menuVisibilityHandle}>
+                                    <a href={item.link} className={`text-[18px] xl:text-[17px] text-white`}>{item.name}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+                    <div className='flex gap-[16px] z-20 justify-center'>
+                        <Link to="#">
+                            <Icon icon="twemoji:flag-poland" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+                        </Link>
+                        <Link to="#">
+                            <Icon icon="twemoji:flag-united-kingdom" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+                        </Link>
+                        <Link to="#">
+                            <Icon icon="twemoji:flag-germany" width="36" height="36" className='hover:scale-105 duration-300 max-xl:w-[28px]' />
+                        </Link>
+                    </div>
+
+                    <button className='w-[250px] h-[50px] rounded-full z-20 bg-[#898989] hover:bg-[#676767] duration-300 cursor-pointer flex justify-center items-center gap-[8px]'>
+                        <Icon icon="solar:letter-bold" width="26" height="26" className="text-white max-xl:w-[24px]" />
+                        <p className='text-white text-[20px] max-xl:text-[16px]'>Napisz do nas</p>
+                    </button>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
