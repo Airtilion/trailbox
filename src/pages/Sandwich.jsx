d087376af7aad6900f788ddbb6f6024145f061a0
JcobV01 <jakubwadycki@gmail.com>
2025-03-18 09:34:13 +0100

    first commit

---
+++ src/pages/Sandwich.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sandwich = () => {
+  return (
+    <div>Sandwich</div>
+  )
+}
+
+export default Sandwich
---
+++ src/routes/Router.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import { lazy, Suspense } from "react";
+import Loading from "../components/Loading";
+
+const Home = lazy(() => import("../pages/Home"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const Sandwich = lazy(() => import("../pages/Sandwich"));
+const Containers = lazy(() => import("../pages/Containers"));
+const Cnc = lazy(() => import("../pages/Cnc"));
+const Portfolio = lazy(() => import("../pages/Portfolio"));
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "plytysandwich", element: <Sandwich /> },
+      { path: "zestawy", element: <Containers /> },
+      { path: "cnc", element: <Cnc /> },
+      { path: "realizacje", element: <Portfolio /> },
+    ],
+  },
+  { path: "*", element: <NotFound /> },
+]);
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  )
+};
+
+export default AppRouter;
