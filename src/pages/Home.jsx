01f535667889d47f9fc6148e682e49651cb1f01f
JcobV01 <jakubwadycki@gmail.com>
2025-03-19 16:47:30 +0100

    sekcja oferta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BusinessDesc from '../components/home/BusinessDesc'
+import Offer from '../components/home/Offer'
 
 const Home = () => {
   return (
     <>
       <BusinessDesc/>
+      <Offer/>
     </>
   )
 }
