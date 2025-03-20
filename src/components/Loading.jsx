7adc42fa972cdad99462227fd8d57718237629e7
JcobV01 <jakubwadycki@gmail.com>
2025-03-20 22:50:48 +0100

    loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import logo from '../assets/images/logo.svg'
+
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <section className='w-full h-dvh flex flex-col justify-center items-center gap-[16px]'>
+        <img src={logo} alt="Logo mentorNGO"  width={200} className='h-[200px]'/>
+        <div className='loader'></div>
+    </section>
   )
 }
 
