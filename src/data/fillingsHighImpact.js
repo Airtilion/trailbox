79c2b115159ff2624982018641d6559811b69a87
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-21 11:28:44 +0100

    High Impact

---
+++ src/data/fillingsHighImpact.js
@@ -0,0 +1,232 @@
+import xps from '../assets/images/sandwich/panels/high-impact/fillings/xps.webp'
+import pet from '../assets/images/sandwich/panels/high-impact/fillings/pet.webp'
+import plaster from '../assets/images/sandwich/panels/high-impact/fillings/plaster-miodu.webp'
+import pp from '../assets/images/sandwich/panels/high-impact/fillings/pp.webp'
+
+import xpsSmall from '../assets/images/sandwich/panels/high-impact/fillings/xps-small.webp'
+import petSmall from '../assets/images/sandwich/panels/high-impact/fillings/pet-small.webp'
+import plasterSmall from '../assets/images/sandwich/panels/high-impact/fillings/plaster-miodu-small.webp'
+import ppSmall from '../assets/images/sandwich/panels/high-impact/fillings/pp-small.webp'
+
+import xps16 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-16mm.webp'
+import xps21 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-21mm.webp'
+import xps25 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-25mm.webp'
+import xps27 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-27mm.webp'
+import xps30 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-30mm.webp'
+import xps40 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-40mm.webp'
+import xps50 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-50mm.webp'
+import xps60 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-60mm.webp'
+import xps80 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-80mm.webp'
+import xps100 from '../assets/images/sandwich/panels/high-impact/cores/xps/xps-100mm.webp'
+
+import xps16Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-16mm-small.webp'
+import xps21Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-21mm-small.webp'
+import xps25Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-25mm-small.webp'
+import xps27Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-27mm-small.webp'
+import xps30Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-30mm-small.webp'
+import xps40Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-40mm-small.webp'
+import xps50Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-50mm-small.webp'
+import xps60Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-60mm-small.webp'
+import xps80Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-80mm-small.webp'
+import xps100Small from '../assets/images/sandwich/panels/high-impact/cores/xps/small/xps-100mm-small.webp'
+
+import pet10 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-10mm.webp'
+import pet15 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-15mm.webp'
+import pet20 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-20mm.webp'
+import pet30 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-30mm.webp'
+import pet40 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-40mm.webp'
+import pet50 from '../assets/images/sandwich/panels/high-impact/cores/pet/pet-50mm.webp'
+
+import pet10Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-10mm-small.webp'
+import pet15Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-15mm-small.webp'
+import pet20Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-20mm-small.webp'
+import pet30Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-30mm-small.webp'
+import pet40Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-40mm-small.webp'
+import pet50Small from '../assets/images/sandwich/panels/high-impact/cores/pet/small/pet-50mm-small.webp'
+
+import plaster5 from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/plaster-miodu-5mm.webp'
+import plaster10 from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/plaster-miodu-10mm.webp'
+import plaster15 from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/plaster-miodu-15mm.webp'
+import plaster20 from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/plaster-miodu-20mm.webp'
+import plaster25 from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/plaster-miodu-25mm.webp'
+
+import plaster5Small from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/small/plaster-miodu-5mm-small.webp'
+import plaster10Small from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/small/plaster-miodu-10mm-small.webp'
+import plaster15Small from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/small/plaster-miodu-15mm-small.webp'
+import plaster20Small from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/small/plaster-miodu-20mm-small.webp'
+import plaster25Small from '../assets/images/sandwich/panels/high-impact/cores/plaster-miodu/small/plaster-miodu-25mm-small.webp'
+
+import pp15 from '../assets/images/sandwich/panels/high-impact/cores/pp/pp-15mm.webp'
+import pp20 from '../assets/images/sandwich/panels/high-impact/cores/pp/pp-20mm.webp'
+import pp30 from '../assets/images/sandwich/panels/high-impact/cores/pp/pp-30mm.webp'
+import pp40 from '../assets/images/sandwich/panels/high-impact/cores/pp/pp-40mm.webp'
+import pp50 from '../assets/images/sandwich/panels/high-impact/cores/pp/pp-50mm.webp'
+
+import pp15Small from '../assets/images/sandwich/panels/high-impact/cores/pp/small/pp-15mm-small.webp'
+import pp20Small from '../assets/images/sandwich/panels/high-impact/cores/pp/small/pp-20mm-small.webp'
+import pp30Small from '../assets/images/sandwich/panels/high-impact/cores/pp/small/pp-30mm-small.webp'
+import pp40Small from '../assets/images/sandwich/panels/high-impact/cores/pp/small/pp-40mm-small.webp'
+import pp50Small from '../assets/images/sandwich/panels/high-impact/cores/pp/small/pp-50mm-small.webp'
+
+const fillingsHighImpact = [
+    {
+        title: "XPS",
+        imgSmall: xpsSmall,
+        imgFull: xps,
+        cores: [
+            {
+                thickness: 16,
+                imgSmall: xps16Small,
+                imgFull: xps16
+            },
+            {
+                thickness: 21,
+                imgSmall: xps21Small,
+                imgFull: xps21
+            },
+            {
+                thickness: 25,
+                imgSmall: xps25Small,
+                imgFull: xps25
+            },
+            {
+                thickness: 27,
+                imgSmall: xps27Small,
+                imgFull: xps27
+            },
+            {
+                thickness: 30,
+                imgSmall: xps30Small,
+                imgFull: xps30
+            },
+            {
+                thickness: 40,
+                imgSmall: xps40Small,
+                imgFull: xps40
+            },
+            {
+                thickness: 50,
+                imgSmall: xps50Small,
+                imgFull: xps50
+            },
+            {
+                thickness: 60,
+                imgSmall: xps60Small,
+                imgFull: xps60
+            },
+            {
+                thickness: 80,
+                imgSmall: xps80Small,
+                imgFull: xps80
+            },
+            {
+                thickness: 100,
+                imgSmall: xps100Small,
+                imgFull: xps100
+            },
+        ]
+    },
+    {
+        title: "PET",
+        imgSmall: petSmall,
+        imgFull: pet,
+        cores: [
+            {
+                thickness: 10,
+                imgSmall: pet10Small,
+                imgFull: pet10
+            },
+            {
+                thickness: 15,
+                imgSmall: pet15Small,
+                imgFull: pet15
+            },
+            {
+                thickness: 20,
+                imgSmall: pet20Small,
+                imgFull: pet20
+            },
+            {
+                thickness: 30,
+                imgSmall: pet30Small,
+                imgFull: pet30
+            },
+            {
+                thickness: 40,
+                imgSmall: pet40Small,
+                imgFull: pet40
+            },
+            {
+                thickness: 50,
+                imgSmall: pet50Small,
+                imgFull: pet50
+            },
+        ]
+    },
+    {
+        title: "Plaster miodu",
+        imgSmall: plasterSmall,
+        imgFull: plaster,
+        cores: [
+            {
+                thickness: 5,
+                imgSmall: plaster5Small,
+                imgFull: plaster5
+            },
+            {
+                thickness: 10,
+                imgSmall: plaster10Small,
+                imgFull: plaster10
+            },
+            {
+                thickness: 15,
+                imgSmall: plaster15Small,
+                imgFull: plaster15
+            },
+            {
+                thickness: 20,
+                imgSmall: plaster20Small,
+                imgFull: plaster20
+            },
+            {
+                thickness: 25,
+                imgSmall: plaster25Small,
+                imgFull: plaster25
+            },
+        ]
+    },
+    {
+        title: "PP",
+        imgSmall: ppSmall,
+        imgFull: pp,
+        cores: [
+            {
+                thickness: 15,
+                imgSmall: pp15Small,
+                imgFull: pp15
+            },
+            {
+                thickness: 20,
+                imgSmall: pp20Small,
+                imgFull: pp20
+            },
+            {
+                thickness: 30,
+                imgSmall: pp30Small,
+                imgFull: pp30
+            },
+            {
+                thickness: 40,
+                imgSmall: pp40Small,
+                imgFull: pp40
+            },
+            {
+                thickness: 50,
+                imgSmall: pp50Small,
+                imgFull: pp50
+            },
+        ]
+    }
+]
+
+export default fillingsHighImpact
--- src/pages/sandwich/HighImpact.jsx
+++ src/pages/sandwich/HighImpact.jsx
@@ -1,25 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ImageSection from '../../components/sandwich/configurator/ImageSection'
 import BackTo from '../../components/sandwich/configurator/BackTo'
+import Type from '../../components/sandwich/configurator/Type'
+import Configurator from '../../components/sandwich/configurator/Configurator'
+
+import highGlossDefault from '../../assets/images/sandwich/panels/high-impact/high-impact.webp'
+import fillings from '../../data/fillingsHighImpact'
+import Description from '../../components/sandwich/configurator/Description'
+
+const aboutPanel = {
+  title: "High Impact",
+  desc: "Płyta warstwowa do zabudowy nadwozi, w skład której wchodzi laminat poliestrowy wzmacniany włóknem szklanym oraz wysokiej jakości wypełnienie.",
+  standardThickness: "Standardowa grubość laminatu to 1 - 1.5 mm",
+  maxSize: "Maksymalna możliwa wielkość panela to 3 m x 9.5 m.",
+  outerCoating: [
+    {
+      content: "Laminat poliestrowo-szklany ma zastosowanie wewnątrz oraz na zewnątrz zabudów."
+    },
+    {
+      content: "Łączy w sobie wyjątkową odporność na uderzenia i wytrzymałość z bardzo niską wagą, a także ma atrakcyjną wizualnie i łatwą do czyszczenia powierzchnię."
+    },
+    {
+      content: "Posiada wysoką odporność na uderzenia przy niewielkiej wadze, jak również doskonałą wytrzymałość na zginanie i rozciąganie."
+    }
+  ],
+  filling: [
+    {
+      content: "XPS 16-100mm, 30 - 40 kg/m3",
+    },
+    {
+      content: "PET 10-50mm, 70 - 250kg kg/m3",
+    },
+    {
+      content: "Plaster miodu 5-25mm, 60 - 80kg kg/m3",
+    },
+    {
+      content: "PP 15-50mm, 80kg kg/m3",
+    }
+  ]
+}
 
 const HighImpact = () => {
+  const [selectedFilling, setSelectedFilling] = useState(null);
+  const [selectedCore, setSelectedCore] = useState(null);
+
+  const selectedFillingObject = fillings.find(filling => filling.title === selectedFilling);
+
+  const selectedImage = selectedFillingObject
+    ? selectedCore
+      ? selectedFillingObject.cores.find(core => core.thickness === selectedCore)?.imgFull || highGlossDefault
+      : selectedFillingObject.imgFull || highGlossDefault
+    : highGlossDefault;
+
+  useEffect(() => {
+    setSelectedCore(null);
+  }, [selectedFilling]);
+
   return (
-    <section>
-      <article className='w-[1130px] mx-auto max-xl:w-[1000px] max-lg:w-[90%]'>
-        <BackTo />
-
-        <div className='flex gap-[64px] items-center justify-center'>
-
-          <div>
-
+    <>
+      <section>
+        <div className='w-[1130px] mx-auto max-xl:w-[1000px] max-lg:w-[90%] max-lg:mb-[87px]'>
+          <div className='max-w-max'>
+            <BackTo />
           </div>
-          <div>
+
+          <div className='mt-[32px] flex gap-[64px] max-lg:flex-col max-lg:items-center'>
+
+            <ImageSection selectedImage={selectedImage} />
+
+            <div className='relative w-[415px] max-lg:w-full'>
+
+              <Type title={aboutPanel.title} />
+              <Configurator fillings={fillings} selectedFilling={selectedFilling} setSelectedFilling={setSelectedFilling} selectedCore={selectedCore} setSelectedCore={setSelectedCore} selectedFillingObject={selectedFillingObject} />
+
+            </div>
 
           </div>
 
         </div>
+      </section>
 
-      </article>
-    </section>
+      <section className='mt-[32px] py-[64px] bg-[#F3F7F8]'>
+        <div className='w-[1130px] mx-auto max-xl:w-[1000px] max-lg:w-[90%]'>
+          <Description aboutPanel={aboutPanel} />
+        </div>
+      </section>
+    </>
   )
 }
 
