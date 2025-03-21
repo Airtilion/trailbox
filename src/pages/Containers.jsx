731476969c00bbea32d9079b2894d10c3bb0a152
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-21 11:34:27 +0100

    Wstęp do ofery zestawy kontenerowe z mediami

--- src/pages/Containers.jsx
+++ src/pages/Containers.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import AboutOffer from '../components/containers/AboutOffer'
 
 const Containers = () => {
   return (
-    <div>Containers</div>
+    <>
+      <AboutOffer />
+    </>
   )
 }
 
