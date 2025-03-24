3a137f997bbc86dd7d681d8e93e726ee9d958ae4
JcobV01 <jakubwadycki@gmail.com>
2025-03-24 09:05:34 +0100

    dodatkowe dane kontaktowe oraz custom checkbox

---
+++ src/components/home/AfterContact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AfterContact = () => {
+  return (
+    <section className='py-[16px] flex justify-center gap-[64px] text-white bg-[#898989]'>
+        <article>
+          <h2 className='text-[25px] font-semibold'>BIURO</h2>
+          <p>Tel. +48 533 468 979</p>
+          <p>a.rasche@trailbox.pl</p>
+        </article>
+
+        <article>
+          <h2 className='text-[25px] font-semibold'>DZIAŁ HANDLOWY</h2>
+          <p>Tel. +48 882 143 803</p>
+          <p>w.rasche@trailbox.pl</p>
+        </article>
+      </section>
+  )
+}
+
+export default AfterContact
--- src/components/home/ContactForm.jsx
+++ src/components/home/ContactForm.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import CustomCheckbox from '../CustomCheckbox'
 
 const ContactForm = ({isVisible}) => {
+    const chRef = useRef()
     return (
         <section className={`flex-1 relative z-10 px-[64px] py-[32px] max-xl:pb-[80px] max-md:px-[32px] max-md:pb-[75px] max-xs:!pb-[78px] transition-all duration-1000 ease-in-out ${isVisible ? 'element-visible-right' : 'element-hidden-right'}`}>
             <h3 className='text-[25px] font-semibold relative z-10 max-md:text-[20px] max-md:mt-[12px]'>Wyślij zapytanie</h3>
@@ -27,14 +29,7 @@ const ContactForm = ({isVisible}) => {
                     <textarea type="text" id="content" placeholder='Dzień dobry...' className='border-b-2 border-[#898989] h-[100px] mt-[16px] outline-none resize-none text-[14px]' required />
                 </div>
 
-                <div className='w-[500px] flex gap-[8px] items-start max-lg:w-full max-lg:mb-[14px]'>
-                    <input type="checkbox" id="rulebook" required className='peer opacity-0 absolute h-0 w-0' />
-                    <label htmlFor="rulebook" className="w-5 h-5 border-2 border-[#898989] flex items-center justify-center cursor-pointer peer-checked:bg-[#898989] peer-checked:border-[#898989] transition-all duration-200">
-                        <span className="hidden peer-checked:block text-white">✓</span>
-                    </label>
-
-                    <p className='text-[10px] leading-[14px] flex-1 max-xs:!text-[9px]'>Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z ustawą o ochronie danych osobowych w związku z wysłaniem zapytania poprzez formularz kontaktowy bądź wysłania informacji handlowej. Jestem świadomy/-a iż przysługuje mi prawo dostępu do swoich danych, możliwości ich przetwarzania oraz żądania zaprzestania ich przetwarzania. Administratorem Państwa danych osobowych jest Trailbox Holding Sp. z o.o. Glinno 3K, 98-290 Warta (NIP: 8272325281, REGON: 387548703, Tel. kontaktowy: +48 882 143 803, e-mail: w.rasche@trailbox.pl). Wszystkie informacje na temat przetwarzania danych znaleźć można w zakładce „Polityka prywatności” na naszej stronie www.trailbox.pl</p>
-                </div>
+                <CustomCheckbox checkboxRef={chRef}/>
             </form>
             <button className='absolute bottom-0 right-0 cursor-pointer text-white text-[20px] font-light rounded-full bg-[#898989] h-[65px] px-[64px] max-xs:!text-[18px] max-xs:!h-[50px] overflow-hidden before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#676767] before:scale-x-0 before:origin-left before:transition-transform before:duration-1000 hover:before:scale-x-100'>                        
                 <span className='relative z-10'>Wyślij</span>
