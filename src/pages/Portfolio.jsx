70b59ccb75eef3612d40f4ad7804811e2e226e21
JcobV01 <jakubwadycki@gmail.com>
2025-03-25 10:36:41 +0100

    animacje w realizacjach

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import CallToAction from '../components/CallToAction';
 import test from '../assets/images/background.webp'
+import useIntersectionObserver from '../hooks/useObserver';
 
 
 const Portfolio = () => {
+  const [ref, isVisible] = useIntersectionObserver();
+
   // Importujemy wszystkie pliki .webp z katalogu assets/images/gallery
   const images = import.meta.glob('/src/assets/images/gallery/*.webp', { eager: true });
   // Zamieniamy obiekt na tablicę poprawnych URLi
@@ -43,15 +46,15 @@ const Portfolio = () => {
 
   return (
     <>
-      <section className='w-[1248px] mx-auto my-[64px] max-2xl:w-[932px] max-lg:w-[616px] max-sm:w-[90%]'>
-        <h2 className='text-[30px] font-semibold w-[450px] max-lg:w-full max-md:text-[20px] max-sm:w-[90%] max-sm:mx-auto'>
+      <section ref={ref} className='w-[1248px] mx-auto my-[64px] max-2xl:w-[932px] max-lg:w-[616px] max-sm:w-[90%]'>
+        <h2 className={`text-[30px] font-semibold w-[450px] max-lg:w-full max-md:text-[20px] max-sm:w-[90%] max-sm:mx-auto transition-all duration-1000 ease-in-out ${isVisible ? 'element-visible-left' : 'element-hidden-left'}`}>
           Realizacje - Galeria zdjęć
         </h2>
-        <p className='text-[16px] font-light flex-1 max-lg:text-[15px] max-md:text-[14px] max-sm:w-[90%] max-sm:mx-auto'>
+        <p className={`text-[16px] font-light flex-1 max-lg:text-[15px] max-md:text-[14px] max-sm:w-[90%] max-sm:mx-auto transition-all duration-1000 ease-in-out ${isVisible ? 'element-visible-left' : 'element-hidden-left'}`}>
           Realizacje naszych klientów to najlepszy dowód na wysoką jakość wykonania oraz indywidualne podejście do każdego projektu.
           Zobacz, jak nasze panele sandwich sprawdzają się w praktyce i przekonaj się, że nasze rozwiązania mogą sprostać Twoim oczekiwaniom.
         </p>
-      
+
         <article className='grid grid-cols-4 gap-[16px] my-[64px] max-2xl:grid-cols-3 max-lg:grid-cols-2 max-sm:flex max-sm:flex-col max-sm:items-center'>
           {visibleImages.map((src, index) => {
             // Ustalamy numer obrazka, licząc od 1 (bo warunki liczymy od 1)
@@ -63,8 +66,8 @@ const Portfolio = () => {
               (imgNumber >= 16 && (imgNumber - 16) % 18 === 0);
 
             const classes = isSpecial
-              ? 'col-span-2 row-span-2 w-[620px] h-[420px] rounded-[5px] object-cover max-sm:w-[300px] max-sm:h-[200px]'
-              : 'w-[300px] h-[200px] rounded-[5px] object-cover';
+              ? 'col-span-2 row-span-2 w-[620px] h-[420px] rounded-[5px] object-cover max-sm:w-[300px] max-sm:h-[200px] animation-show'
+              : 'w-[300px] h-[200px] rounded-[5px] object-cover animation-show';
 
             return (
               <img
@@ -80,10 +83,10 @@ const Portfolio = () => {
         {/* Element "sentinel" do infinite scroll */}
         <div ref={sentinelRef} className='h-1'></div>
       </section>
-      <CallToAction/>
-        <div className='fixed z-[-1] w-full h-dvh top-0 left-0 bg-white'>
-            <img src={test} alt="" className='w-full h-full object-cover opacity-10'/>
-          </div>
+      <CallToAction />
+      <div className='fixed z-[-1] w-full h-dvh top-0 left-0 bg-white'>
+        <img src={test} alt="" className='w-full h-full object-cover opacity-10' />
+      </div>
     </>
   );
 };
