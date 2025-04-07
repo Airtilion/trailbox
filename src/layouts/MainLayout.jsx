8e8dd8f93231969a5f55f1fd15f074f56db58d15
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-04-07 11:29:04 +0200

    Drobne zmiany w layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,18 +2,12 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-import plyty_header from '../assets/images/headers/header-plyty.webp'
 import plyty_cnc from '../assets/images/cnc_photo.webp'
 import plyty_realizacje from '../assets/images/headers/realizacje-bg.webp'
 import plyty_zestawy from '../assets/images/headers/zestawy_bg.webp'
 import ScrollToSection from "../components/ScrollToSection";
 
 const headers = [
-    {
-        title: 'PŁYTY TYPU SANDWICH',
-        subtitle: 'Innowacyjna technologia łącząca lekkość z wytrzymałością',
-        image: plyty_header
-    },
     {
         title: 'GOTOWE ZESTAWY KONTENEROWE',
         subtitle: 'Funkcjonalne i modułowe rozwiązania dla każdej przestrzeni',
@@ -37,7 +31,7 @@ const MainLayout = () => {
 
     return (
         <>
-            <Header key={location.pathname} data={path.startsWith('/plyty-sandwich') ? 'ps' : path === '/cnc' ? headers[2] : path === '/realizacje' ? headers[3] : path === '/zestawy' ? headers[1] : ''}/>
+            <Header key={location.pathname} data={path.startsWith('/plyty-sandwich') ? 'ps' : path === '/cnc' ? headers[1] : path === '/realizacje' ? headers[2] : path === '/zestawy' ? headers[0] : ''}/>
             <main className="flex flex-col relative overflow-hidden">
                 <Outlet />
             </main>
